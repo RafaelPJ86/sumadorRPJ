d10a17e32e013abf12c8727c81834283216b7867
Rafael Piñol <r.pinyol.jarenyo@gmail.com>
2019-03-04 18:30:30 +0100

    Inicio Preparado

---
+++ test/sumador/TestAsumar.java
@@ -0,0 +1,14 @@
+package sumador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAsumar {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
