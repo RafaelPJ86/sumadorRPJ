901495ea79af56deb617a725b91132756d0fbf85
Rafael Piñol <r.pinyol.jarenyo@gmail.com>
2019-03-04 22:38:37 +0100

    No consigo que funcione

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -14,29 +14,47 @@ public class ASumar {
 	public ASumar(String string) {
 		this.valor_inicial = string;
 	}
-
-	/**
-	 * @return
-	 * si numero.length == 1
-	 */
-	public String mostrar() {
-        String numero = valor_inicial;
-        String cadena="";
-        int numEntero = Integer.parseInt(numero);
-        if (numEntero > 0) {
-        	 if (numero.length() == 1) {
-             	
-                 cadena= (numero + " = " + numero);
-             }
-        	
-        	
-        }else {
-        	cadena= "";
-        	
-        }
- 		return cadena;
-
-		
-}
 	
+	
+	
+	
+	 /**
+	 * @return
+	 */
+	public int total() {
+	        int suma = 0;
+
+	        for (int i = 0; i < valor_inicial.length(); i++) {
+	            // Cadena de un dígito.
+	            String digito = valor_inicial.substring(i, 1);
+	            suma = suma + Integer.parseInt(digito);
+	        }
+	        return suma;
+
+	    }
+	 
+	 /**
+	 * @return
+	 */
+	public String expresion() {
+	        String suma = "";
+
+	        for (int i = 0; i < valor_inicial.length(); i++) {
+	            // Cadena de un dígito.
+	            String digito = valor_inicial.substring(i, 1);
+	            suma = suma + digito + " + "; //Atención puede que así no funcione**
+	        }
+	        return suma;
+
+	    }
+
+
+        	
+            
+
+
+        	
+        	
+     
+		
 }
--- test/sumador/TestAsumar.java
+++ test/sumador/TestAsumar.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.Test;
 class TestAsumar {
 
 	@Test
-    public void testNegativo() {
-        ASumar sumi = new ASumar("-1");
-        String resultado = sumi.mostrar();
-        assertEquals("",resultado );
+    public void testNumero() {
+        ASumar sumi = new ASumar("22");
+        String expresion = sumi.expresion();
+        int total1 =sumi.total();
+        String total2= String.valueOf(total1);
+        String resultado=expresion + "=" +total2;  
+
+        assertEquals("2+2=4", resultado);
     }
 
 }
