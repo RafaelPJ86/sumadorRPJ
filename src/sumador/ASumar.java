f54c0aee120623e5e650ac0ef7c67e383d17dc9f
Rafael Piñol <r.pinyol.jarenyo@gmail.com>
2019-03-04 19:11:48 +0100

    Correcto

---
+++ src/sumador/ASumar.java
@@ -0,0 +1,33 @@
+package sumador;
+
+/**
+ * @author usuario
+ *
+ */
+public class ASumar {
+
+	private String valor_inicial;
+
+	/**
+	 * @param string
+	 */
+	public ASumar(String string) {
+		this.valor_inicial = string;
+	}
+
+	/**
+	 * @return
+	 * si numero.length == 1 devuelve el string si no devuelve null.
+	 */
+	public String mostrar() {
+        String numero = valor_inicial;
+        String cadena="";
+        if (numero.length() == 1) {
+        	
+            cadena= (numero + " = " + numero);
+        }
+		return cadena;
+		
+}
+	
+}
